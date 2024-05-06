13881e1b3ef0f1305daba08c34a1bb34aecf4214
dftyghuj8 <js1aaronjac@hotmail.com>
2024-05-06 10:17:36 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) return true;
+function longestConsecutive(nums) {
+  const set = new Set(nums);
+  let longest = 0;
+  for (const num of set) {
+    if (!set.has(num - 1)) {
+      let currentNum = num;
+      let currentStreak = 1;
+      while (set.has(currentNum + 1)) {
+        currentNum++;
+        currentStreak++;
+      }
+      longest = Math.max(longest, currentStreak);
+    }
   }
-  return false;
+  return longest;
 }
